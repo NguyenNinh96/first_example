9c3f8815773921ace674f5c09a68c1dd42f9eb16
Nguyen Ninh <ninh.nt@anlab.pro>
2017-06-20 09:38:11 +0700

    hi

---
+++ vidu_1a.php
@@ -0,0 +1,15 @@
+﻿<?php echo "hello world <br>"; 
+
+$username = "root";
+$password = "";
+$host = "localhost";
+$database = "vidu_1";
+$connection = new mysqli($host, $username, $password, $database);
+//mysql_query("set names 'utf8'");
+$sql = "select * from bang1";
+$query = $connection->query($sql);
+
+while($row = $query->fetch_assoc()){
+	echo $row["STT"]."-".$row["Name"]."-".$row["Age"]."<br>";
+}
+?>
---
+++ vidu_1b.php
@@ -0,0 +1,11 @@
+﻿<?php 
+mysql_connect("localhost","root","") or die ("no connect");
+mysql_select_db("vidu_1");
+mysql_query("set names 'utf8'");
+$sql = "select * from bang1";
+$query = mysql_query($sql);
+while($row = mysql_fetch_array($query)){
+	echo $row["STT"]."-".$row["Name"]."-".$row["Age"]."<br>";
+}
+mysql_close();
+?>
